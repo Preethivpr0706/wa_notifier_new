09f96793b1a499520ad42f719bff32bf67318b46
Preethi V <preethivijay0706@gmail.com>
2025-09-24 22:29:07 +0530

    temp ad

---
+++ frontend/src/components/EditTemplate/templateConstants.jsx
@@ -0,0 +1,22 @@
+export const CATEGORIES = [
+    { value: 'marketing', label: 'Marketing' },
+    { value: 'utility', label: 'Utility' },
+  ];
+  
+  export const LANGUAGES = [
+    { value: 'en', label: 'English' },
+    { value: 'en_US', label: 'English (US)' },
+    { value: 'es_ES', label: 'Spanish (Spain)' },
+    { value: 'fr_FR', label: 'French (France)' },
+    { value: 'pt_BR', label: 'Portuguese (Brazil)' },
+    { value: 'de_DE', label: 'German (Germany)' },
+    { value: 'it_IT', label: 'Italian (Italy)' },
+    { value: 'ru_RU', label: 'Russian (Russia)' },
+    { value: 'ja_JP', label: 'Japanese (Japan)' },
+    { value: 'ko_KR', label: 'Korean (Korea)' },
+    { value: 'zh_CN', label: 'Chinese (China)' },
+    { value: 'zh_HK', label: 'Chinese (Hong Kong)' },
+    { value: 'zh_TW', label: 'Chinese (Taiwan)' },
+    { value: 'ar_AR', label: 'Arabic' },
+    { value: 'he_IL', label: 'Hebrew (Israel)' }
+  ];
